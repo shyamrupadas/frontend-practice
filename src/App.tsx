219a1e9bce300158ec8f894edd32a722bb6d3bf9
Sergey Maksimov <s.mksimov@gmail.com>
2024-11-12 23:40:01 +0300

    Fix react-router-dom warnings

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,14 @@ import './App.css'
 import { Routes } from './Routes'
 
 function App() {
-  return <RouterProvider router={Routes} />
+  return (
+    <RouterProvider
+      router={Routes}
+      future={{
+        v7_startTransition: true,
+      }}
+    />
+  )
 }
 
 export default App
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -8,18 +8,29 @@ import { Maria } from './pages/Maria'
 import { VyacheslavTask1 } from './pages/Vyacheslav/VyacheslavTask1'
 import { MaximTask1 } from './pages/Maxim/MaximTask1'
 
-export const Routes = createBrowserRouter([
+export const Routes = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Main />,
+    },
+    { path: '/reference', element: <Reference /> },
+    {
+      path: '/vyacheslav',
+      element: <Vyacheclav />,
+    },
+    { path: '/vyacheslav/first-task', element: <VyacheslavTask1 /> },
+    { path: '/maxim', element: <Maxim /> },
+    { path: '/maxim/first-task', element: <MaximTask1 /> },
+    { path: '/maria', element: <Maria /> },
+  ],
   {
-    path: '/',
-    element: <Main />,
-  },
-  { path: '/reference', element: <Reference /> },
-  {
-    path: '/vyacheslav',
-    element: <Vyacheclav />,
-  },
-  { path: '/vyacheslav/first-task', element: <VyacheslavTask1 /> },
-  { path: '/maxim', element: <Maxim /> },
-  { path: '/maxim/first-task', element: <MaximTask1 /> },
-  { path: '/maria', element: <Maria /> },
-])
+    future: {
+      v7_fetcherPersist: true,
+      v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
+      v7_relativeSplatPath: true,
+      v7_skipActionErrorRevalidation: true,
+    },
+  }
+)
